31720372fc19d759936f1e3d080f20f743693019
Kai <kai.niu@ibm.com>
2019-08-07 18:40:30 -0700

    working on add namespace concepts to feature management

--- test/providers/meta_manager/test_meta_manager_base.py
+++ test/providers/meta_manager/test_meta_manager_base.py
@@ -30,3 +30,74 @@ class TestMetaManagerBase():
         with pytest.raises(NotImplementedError):
             base_class.remove_feature(None)
 
+    def test_build_canonical_namespace_method_default_namespace(self, base_class):
+        # arrange
+        manager = base_class
+        value_list = [None,'','       ']
+        # act
+        result_list = [manager.__build_canonical_namespace__(val) for val in value_list]
+        # assert
+        for val in result_list:
+            assert val == ((0,'default'),)
+
+    def test_build_canonical_namespace_method_raise_exception(self, base_class):
+        # arrange
+        value_list = ['.','..',' . . . ','a-.bc','foo.foo','foo.bar.foo']
+        # act/assert
+        for val in value_list:
+            with pytest.raises(Exception):
+                base_class.__build_canonical_namespace__(val)
+
+    def test_build_canonical_namespace_method_expected_result(self, base_class):
+        # arrange
+        value_list = ['foo.bar','default.foo.Bar']
+        expected_namespace_list = [
+            ((0,'foo'),(1,'bar')),
+            ((0,'default'),(1,'foo'),(2,'bar'))
+        ]
+        # act
+        namespace_list = [base_class.__build_canonical_namespace__(val) for val in value_list]
+        # assert
+        for index, ns in enumerate(expected_namespace_list):
+            assert len(ns) == len(namespace_list[index])
+            for part in ns:
+                assert part in namespace_list[index]
+
+    def test_namespace_match_method_expected_false_return(self, base_class):
+        # arrange
+        ns1_list = [
+            None,
+            ((0,'foo'),(1,'bar')),
+            ((0,'foo'),(1,'bar')), 
+            ((0,'foo'))
+            ]
+        ns2_list = [
+            ((0,'foo'),(1,'bar')),
+            None,
+            ((0,'foo')),
+            ((0,'bar'),(1,'foo'))
+            ]
+        # act
+        result_list = [base_class.__namespace_match__(ns1_list[index],ns2_list[index]) for index in range(len(ns1_list))]
+        # assert
+        for result in result_list:
+            assert result == False
+
+    def test_namespace_match_method_expected_true_return(self, base_class):
+        # arrange
+        ns1_list = [
+            ((0,'foo'),),
+            ((0,'bar'),(1,'foo')),
+            ((0,'default'),)
+            ]
+        ns2_list = [
+            ((0,'foo'),(1,'bar')),
+            ((0,'bar'),(1,'foo')),
+            ((0,'default'),(1,'foo'))
+        ]
+        # act
+        result_list = [base_class.__namespace_match__(ns1_list[index],ns2_list[index]) for index in range(len(ns1_list))]
+        # assert
+        for result in result_list:
+            assert result == True
+
--- test/providers/persistor/test_ibm_object_storage_persistor.py
+++ test/providers/persistor/test_ibm_object_storage_persistor.py
@@ -4,8 +4,8 @@
 import pytest
 from unittest import mock
 from nebula.providers.persistor.ibm_object_storage_persistor import IBMObjectStoragePersistor
-from nebula.core.feature_meta import FeatureMeta
-import ibm_boto3
+from nebula.core import FeatureMetaData
+
 
 @pytest.fixture(scope='function')
 def mock_class(request):
@@ -17,44 +17,158 @@ def mock_class(request):
 @pytest.fixture(scope='function')
 def mock_feature_meta(request):
     # mock the feature meta data class
-    meta = FeatureMeta('test')
+    meta = FeatureMetaData('test')
     meta.author ='test author'
     return meta
+
+@pytest.fixture(scope='function')
+def mock_config(request):
+    # mock the config object
+    config = {
+                "iam_service_id": "foobar_iam_service_id",
+                "ibm_api_key_id": "foobar_ibm_api_key_id",
+                "endpoint": "foobar_endpoint",
+                "ibm_auth_endpoint": "foobar_ibm_auth_endpoint",
+                "bucket": "foobar_bucket"
+             }
+            
+    return config
 
 
 
 class TestIBMObjectStoragePersistorBase():
-
-    def test_init_call_boto_client_once(self, mock_feature_meta):
+    def test_init_parse_config(self, mock_config):
         with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
-            config = '{}'
-            IBMObjectStoragePersistor(config)  
+            # arrange
+            config = mock_config
+            mock.patch.object(mocked_boto,'client', return_value=None)  
+            # act
+            obj = IBMObjectStoragePersistor(config)
+           
+            # assert
+            assert obj.IAM_SERVICE_ID == 'foobar_iam_service_id'
+            assert obj.IBM_API_KEY_ID == 'foobar_ibm_api_key_id'
+            assert obj.ENDPOINT == 'foobar_endpoint'
+            assert obj.IBM_AUTH_ENDPOINT == 'foobar_ibm_auth_endpoint'
+            assert obj.BUCKET == 'foobar_bucket'
+
+    def test_init_call_boto_client_once(self, mock_config):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
+            # arrange
+            config = mock_config
+            # act
+            IBMObjectStoragePersistor(config) 
+            # assert
             mocked_boto.client.assert_called_once()
 
-    def test_write_method_call_put_object_once(self, mock_feature_meta):
+    def test_init_call_boto_client_success(self, mock_config):
         with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
-            # create persistor instance
-            config = '{}'
+            # arrange
+            config = mock_config
+            mocked_boto.client.return_value='client'   
+            # act
             mock_persistor = IBMObjectStoragePersistor(config)
-            # mock boto client object
+            # assert
+            assert mock_persistor.client == 'client'
+
+    def test_init_call_boto_client_exception(self, mock_config, capsys):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
+            # arrange
+            config = mock_config
+            mocked_boto.client.side_effect = Exception('Test Exception')
+            # act
+            IBMObjectStoragePersistor(config)
+            captured = capsys.readouterr()
+            # assert
+            assert captured.out[:5] == '> ibm'
+
+    def test_write_method_call_put_object_once(self, mock_config, mock_feature_meta):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
+            # arrange
+            config = mock_config
+            mock_persistor = IBMObjectStoragePersistor(config)
             mock.patch.object(mocked_boto.client,'put_object',return_value=None)
-            # test
-            mock_persistor.write(mock_feature_meta, None)    
+            # act
+            mock_persistor.write(mock_feature_meta, None) 
+            # assert
             mock_persistor.client.put_object.assert_called_once()
     
-    def test_write_method_call_put_object_exception(self, mock_feature_meta):
+    def test_write_method_call_put_object_exception(self, mock_config, mock_feature_meta, capsys):
         with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3'):
-            # create persistor instance
-            config = '{}'
+            # arrange
+            config = mock_config
             mock_persistor = IBMObjectStoragePersistor(config)
-            # mock boto client object
             mock_persistor.client.put_object.side_effect = Exception('Test Exception')
     
-            # test
+            # act/assert
             with pytest.raises(Exception): 
                 mock_persistor.client.put_object()
-
             mock_persistor.write(mock_feature_meta, None) # supposed to catch exception
+            captured = capsys.readouterr()
+            assert captured.out[:5] == '> ibm'
+
+    def test_read_method_call_get_object_once(self, mock_config, mock_feature_meta):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
+            # arrange
+            config = mock_config
+            mock_persistor = IBMObjectStoragePersistor(config)
+            mock.patch.object(mocked_boto.client,'get_object',return_value=None)
+            # act
+            mock_persistor.read(mock_feature_meta.uid) 
+            # assert   
+            mock_persistor.client.get_object.assert_called_once()
+
+    def test_read_method_call_get_object_return(self, mock_config, mock_feature_meta):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3'):
+            # arrange
+            config = mock_config
+            mock_persistor = IBMObjectStoragePersistor(config)
+            mock_body = mock.Mock()
+            mock_body.read.return_value = b'test body!'
+            mock_persistor.client.get_object.return_value = {'Body':mock_body}
+            # act
+            body = mock_persistor.read(mock_feature_meta.uid)
+            # assert
+            assert body == b'test body!'
+    
+    def test_read_method_call_get_object_exception(self, mock_config, mock_feature_meta, capsys):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3'):
+            # arrange
+            config = mock_config
+            mock_persistor = IBMObjectStoragePersistor(config)
+            mock_persistor.client.get_object.side_effect = Exception('Test Exception')
+            # act/assert
+            with pytest.raises(Exception): 
+                mock_persistor.client.get_object()
+            body = mock_persistor.read(mock_feature_meta.uid) # supposed to catch exception
+            captured = capsys.readouterr()
+            assert captured.out[:5] == '> ibm'
+            assert body == None
+    
+    def test_delete_method_call_delete_object_once(self, mock_config, mock_feature_meta):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3') as mocked_boto:
+            # arrange
+            config = mock_config
+            mock_persistor = IBMObjectStoragePersistor(config)
+            mock.patch.object(mocked_boto.client,'delete_object',return_value=None)
+            # act
+            mock_persistor.delete(mock_feature_meta.uid)    
+            # assert
+            mock_persistor.client.delete_object.assert_called_once()
+
+    def test_delete_method_call_delete_object_exception(self, mock_config, mock_feature_meta, capsys):
+        with mock.patch('nebula.providers.persistor.ibm_object_storage_persistor.ibm_boto3'):
+            # arrange
+            config = mock_config
+            mock_persistor = IBMObjectStoragePersistor(config)
+            mock_persistor.client.delete_object.side_effect = Exception('Test Exception')
+            # act/assert
+            with pytest.raises(Exception): 
+                mock_persistor.client.delete_object()
+            mock_persistor.delete(mock_feature_meta.uid) # supposed to catch exception
+            captured = capsys.readouterr()
+            assert captured.out[:5] == '> ibm'
+
     
 
 
