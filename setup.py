71e9c1592ba5d788001cde558b27470ba767ac0c
Kai <kai.niu@ibm.com>
2019-12-15 20:23:31 -0800

    add pypl package release code

--- setup.py
+++ setup.py
@@ -1,50 +1,56 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import print_function
 
-import io
-import re
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
+"""
+"
+" publish package to pypl index
+" docu: https://realpython.com/pypi-publish-python-package/
+" docu: https://packaging.python.org/tutorials/packaging-projects/
+"
+"""
+from setuptools import setup, Command, find_packages
+import os
 
-from setuptools import find_packages
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-def read(*names, **kwargs):
-    return io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ).read()
-
+"""
+"
+" clean up build files. e.g. python setup.py clean
+" docu: https://stackoverflow.com/questions/3779915/why-does-python-setup-py-sdist-create-unwanted-project-egg-info-in-project-r
+"
+"""
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./src/*.egg-info ./*.pyc ./*.tgz ./*.egg-info')
 
 setup(
-    name='nebula',
-    version='0.0.1',
+    name='DimStore',
+    version='0.0.11',
     license='BSD 2-Clause License',
-    description='Nebula feature store',
     author='Kai Niu',
     author_email='kai.niu@ibm.com',
-    url='http://github.ibm.com/',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    include_package_data=True,
-    zip_safe=False,
+    description='an implementation of feature store',
+    long_description='###an implementation of feature store',
+    long_description_content_type="text/markdown",
+    url=" ",
+    packages= find_packages(where='./src'),
+    package_dir={
+        '': 'src',
+    },
+    keywords=[
+        'feature store', 'data science', 'pipeline'
+    ],
+    zip_safe=True,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -52,27 +58,34 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        'Topic :: Utilities' 
     ],
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
+    python_requires='>=3.6',
     install_requires=[
-        'click',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        'dill ~= 0.2.9',
+        'pickleshare ~= 0.7.5',
+        'botocore ~= 1.12.201', 
+        'ibm-cos-sdk ~= 2.5.2',
+        'ibm-cos-sdk-core ~= 2.5.2',
+        'ibm-cos-sdk-s3transfer ~= 2.5.2'
     ],
     extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
+        'dev' : [''],
+        'test' : ['pytest', 'pytest-cov','mock']
     },
-    entry_points={
-        'console_scripts': [
-            'nameless = nameless.cli:main',
-        ]
-    },
+    cmdclass={
+        'clean': CleanCommand,
+    }
 )
+
+
+""""
+To build package:
+1. move to project root directory
+2. python3 setup.py sdist bdist_wheel
+3. check dist/ folder
+4. python3 -m pip install --user --upgrade twine (optional)
+5. python3 -m twine upload dist/*
+6. python3 -m setup.py clean
+"""
